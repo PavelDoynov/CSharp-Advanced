0cb76d3c03d266bde25e516e3f1a2eb3e8bfb8f9
Pavel Doynov <paveldoynov@Pavels-MacBook-Pro.local>
2018-02-07 18:32:47 +0200

    C# Advanced course

---
+++ Exam preparation/Exam_25_07_2017/02.Knight_Game/KnightGame.cs
@@ -0,0 +1,183 @@
+﻿/*
+ * 02. Knight Game
+ * 
+ * Chess is the oldest game, but it is still popular these days. For this task we will use only one chess
+ * piece – the Knight.
+ * 
+ * The knight moves to the nearest square but not on the same row, column, or diagonal. 
+ * (This can be thought of as moving two squares horizontally, then one square vertically, 
+ * or moving one square horizontally then two squares vertically— i.e. in an "L" pattern.) 
+ * 
+ * The knight game is played on a board with dimensions N x N and a lot of chess knights 0 <= K <= N2. 
+ * 
+ * You will receive a board with K for knights and '0' for empty cells. Your task is to remove a minimum of the knights,
+ * so there will be no knights left that can attack another knight. 
+ * 
+ * Input
+ * On the first line, you will receive the N size of the board.
+ * On the next N lines you will receive strings with Ks and 0s.
+ * 
+ * Output
+ * Print a single integer with the minimum amount of knights that needs to be removed.
+ * 
+ * Constraints
+ * •   Size of the board will be 0 < N < 30
+ * •   Time limit: 0.3 sec. Memory limit: 16 MB.
+ * 
+ * Example
+ * Input:       Output:        Input:       Output:          Input:       Output:
+ * 8            12             5            1                2            0
+ * 0K0KKK00                    0K0K0                         KK
+ * 0K00KKKK                    K000K                         KK
+ * 00K0000K                    00K00
+ * KKKKKK0K                    K000K
+ * K0K0000K                    0K0K0
+ * KK00000K
+ * 00K0K000
+ * 000K00KK
+ * 
+ * https://github.com/PavelDoynov
+ */
+
+
+using System;
+
+namespace _02.Knight_Game
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool[,] chessMatrix = GetMatrix();
+
+            int dangerousKnight = 0;
+            int mostDangerousKnight = 0;
+            int dangerRow = 0;
+            int dangerCol = 0;
+            int removedKnights = 0;
+
+            while (true)
+            {
+                for (int row = 0; row < chessMatrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < chessMatrix.GetLength(1); col++)
+                    {
+                        if (chessMatrix[row, col] == true)
+                        {
+                            if (row - 1 >= 0 && col - 2 >= 0)
+                            {
+                                if (chessMatrix[row - 1, col - 2] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+
+                            if (row + 1 < chessMatrix.GetLength(0) && col - 2 >= 0)
+                            {
+                                if (chessMatrix[row + 1, col - 2] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+
+                            if (row - 1 >= 0 && col + 2 < chessMatrix.GetLength(1))
+                            {
+                                if (chessMatrix[row - 1, col + 2] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+
+                            if (row + 1 < chessMatrix.GetLength(0) && col + 2 < chessMatrix.GetLength(1))
+                            {
+                                if (chessMatrix[row + 1, col + 2] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+
+                            if (row + 2 < chessMatrix.GetLength(0) && col + 1 < chessMatrix.GetLength(1))
+                            {
+                                if (chessMatrix[row + 2, col + 1] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+
+                            if (row - 2 >= 0 && col + 1 < chessMatrix.GetLength(1))
+                            {
+                                if (chessMatrix[row - 2, col + 1] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+
+                            if (row - 2 >= 0 && col - 1 >= 0)
+                            {
+                                if (chessMatrix[row - 2, col - 1] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+
+                            if (row + 2 < chessMatrix.GetLength(0) && col - 1 >= 0)
+                            {
+                                if (chessMatrix[row + 2, col - 1] == true)
+                                {
+                                    dangerousKnight++;
+                                }
+                            }
+                        }
+                        if (dangerousKnight > mostDangerousKnight)
+                        {
+                            mostDangerousKnight = dangerousKnight;
+                            dangerRow = row;
+                            dangerCol = col;
+                        }
+                        dangerousKnight = 0;
+                    }
+                }
+
+                if (mostDangerousKnight > 0) 
+                {
+                    chessMatrix[dangerRow, dangerCol] = false;
+                    removedKnights++;
+                    mostDangerousKnight = 0;
+                }
+                else if (mostDangerousKnight == 0) 
+                {
+                    Console.WriteLine(removedKnights);
+                    break;
+                }
+            }
+        }
+
+        private static bool[,] GetMatrix()
+        {
+
+            int dimensions = int.Parse(Console.ReadLine());
+
+            bool[,] matrix = new bool[dimensions, dimensions];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (input[col] == 'K')
+                    {
+                        matrix[row, col] = true;
+                    }
+                    else
+                    {
+                        matrix[row, col] = false;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
+
---
+++ Exam preparation/Exam_25_07_2017/04.Hospital/Hospital.cs
@@ -0,0 +1,180 @@
+﻿/*
+ * 04. Hospital
+ * 
+ * Your task will be to prepare an electronic register for a hospital. In the hospital we have different departments:
+ * •   Cardiology
+ * •   Oncology
+ * •   Emergency department 
+ * •   etc. 
+ * 
+ * Each department has 20 rooms for patients and each room has 3 beds. When a new patient goes in the hospital, 
+ * he/she is placed on the first free bed in the department. If there are no free beds, the patient should go in another
+ * hospital. Of course, in every hospital there are doctors. Each doctor can have patients in a different department. 
+ * You will receive information about patients in the format {Department} {Doctor} {Patient}
+ * 
+ * After the "Output" command you will receive some other commands with what kind of output you need to print. 
+ * The commands are: 
+ * •   {Department} – You need to print all patients in this department in the order of receiving
+ * •   {Department} {Room} – You need to print all patients in this room in alphabetical order
+ * •   {Doctor} – you need to print all patients for this doctor in alphabetical order
+ * 
+ * The program ends when you receive command "End".
+ * 
+ * Input
+ * On the first lines you will receive info for the hospital, department, doctors and patients in the following format:
+ *                {Department} {Doctor} {Patient}
+ * When you read the "Output" line you will get one or more commands telling you what you need to print
+ * Read commands for printing, ‘till you reach the command "End"
+ * 
+ * Output
+ * •   {Department} – print all patients in this department in order of receiving on new line
+ * •   {Department} {Room} – print all patients in this room in alphabetical order each on new line
+ * •   {Doctor} – print all patients that are healed from doctor in alphabetical order on new line
+ * 
+ * Constraints
+ * •   {Department} – single word with length 1 < n < 100
+ * •   {Doctor} – name and surname, both with length 1 < n < 20
+ * •   {Patient} – unique name with length 1 < n < 20
+ * •   {Room} – integer 1 <= n <= 20
+ * •   Time limit: 0.3 sec. Memory limit: 16 MB.
+ * 
+ * Example:
+ * Input:                                  Output:               Input:                                  Output:
+ * Cardiology Petar Petrov Ventsi          Ventsi                Cardiology Petar Petrov Ventsi          Esmeralda
+ * Oncology Ivaylo Kenov Valio             Simon                 Oncology Ivaylo Kenov Valio             Simon
+ * Emergency Mariq Mircheva Simo           Esmeralda             Emergency Mariq Mircheva Simo           Ventsi
+ * Cardiology Genka Shikerova Simon                              Cardiology Genka Shikerova Simon
+ * Emergency Ivaylo Kenov NuPogodi                               Emergency Ivaylo Kenov NuPogodi
+ * Cardiology Gosho Goshov Esmeralda                             Cardiology Gosho Goshov Esmeralda
+ * Oncology Gosho Goshov Cleopatra                               Oncology Gosho Goshov Cleopatra
+ * Output                                                        Output
+ * Cardiology                                                    Cardiology 1
+ * End                                                           End
+ * 
+ * Input:                                  Output:
+ * Cardiology Petar Petrov Ventsi          NuPogodi
+ * Oncology Ivaylo Kenov Valio             Valio
+ * Emergency Mariq Mircheva Simo
+ * Cardiology Genka Shikerova Simon
+ * Emergency Ivaylo Kenov NuPogodi
+ * Cardiology Gosho Goshov Esmeralda
+ * Oncology Gosho Goshov Cleopatra
+ * Output
+ * Ivaylo Kenov 
+ * End
+ * 
+ * https://github.com/PavelDoynov
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Hospital
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<string[]>> hospital = FillHospital();
+
+            string[] command = Console.ReadLine()
+                                      .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                      .ToArray();
+
+            while (command[0] != "End")
+            {
+                if (command.Count() == 1 && hospital.ContainsKey(command[0]))
+                {
+                    Action<string[]> patient = n => Console.WriteLine($"{n[1]} ");
+
+                    hospital.Where(x => x.Key == command[0])
+                            .SelectMany(sel => sel.Value)
+                            .ToList()
+                            .ForEach(patient);
+
+                    // -------- Or can use this -------------------------------------------
+                    //foreach (var department in hospital.Where(n => n.Key == command[0]))
+                    //{
+                    //    Action<string[]> patient = n => Console.WriteLine($"{n[1]} ");
+
+                    //    department.Value.ForEach(patient); 
+                    //}
+                    //---------------------------------------------------------------------
+                }
+                else if (command.Count() == 2) 
+                {
+                    try
+                    {
+                        string department = command[0];
+                        int room = int.Parse(command[1]);
+
+                        List<string> patients = new List<string>();
+                        foreach (var dep in hospital.Where(n => n.Key == department)) 
+                        {
+                            foreach (var pat in dep.Value) 
+                            {
+                                patients.Add(pat[1]);
+                            }
+                        }
+
+                        patients.Skip((room * 3) - 3).Take(3).OrderBy(name => name).ToList()
+                                .ForEach(name => Console.WriteLine(name));
+                    }
+                    catch (Exception)
+                    {
+                        string doctor = $"{command[0]} {command[1]}";
+                        List<string> patients = new List<string>();
+
+                        foreach (var department in hospital)
+                        {
+                            foreach (var docs in department.Value.Where(a => a[0] == doctor)) 
+                            {
+                                patients.Add(docs[1]);
+                            }
+                        }
+
+                        patients.OrderBy(name => name).ToList().ForEach(name => Console.WriteLine(name));
+                    }
+                }
+                command = Console.ReadLine()
+                                      .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                      .ToArray();
+            }
+        }
+
+        private static Dictionary<string, List<string[]>> FillHospital()
+        {
+            Dictionary<string, List<string[]>> hospital = new Dictionary<string, List<string[]>>();
+
+            string[] input = Console.ReadLine()
+                                    .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+            
+            while (input[0] != "Output")
+            {
+                string department = input[0];
+                string doctor = $"{input[1]} {input[2]}";
+                string patient = input[3];
+
+                if (!hospital.ContainsKey(department))
+                {
+                    hospital[department] = new List<string[]>();
+                    string[] hostipalData = new string[] { doctor, patient };
+                    hospital[department].Add(hostipalData);
+                }
+                else
+                {
+                    string[] hostipalData = new string[] { doctor, patient };
+                    hospital[department].Add(hostipalData);
+                }
+
+                input = Console.ReadLine()
+                                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+            }
+            return hospital;
+        }
+    }
+}
